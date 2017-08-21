87de4a9bdae03f3dbc1d79487bb4566b3eb63720
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-08-21 16:46:34 +0200

    events

--- bot.js
+++ bot.js
@@ -8,22 +8,166 @@ app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
 
-app.get('/bot/forward', function(req, res) {
-    res.sendFile(__dirname + '/public/forward.js');
+
+
+app.get('/bot/forward', function() {
+    let five = require("johnny-five");
+
+
+    let max_speed_l = 150;
+    let max_speed_r = 140;
+
+    // set up the input
+    let stdin = process.stdin;
+
+    stdin.setRawMode(true);
+
+    let board = new five.Board({ port: '/dev/ttyUSB0' });
+
+    let l_motor = r_motor = null;
+
+    board.on("ready", function(err) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+        r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+
+        l_motor.reverse(100);
+        r_motor.forward(100);
+
+    })
+    process.exit(1);
 });
 
-app.get('/bot/backward', function(req, res) {
-    res.sendFile(__dirname + '/public/backward.js');
+app.get('/bot/backward', function() {
+    let five = require("johnny-five");
+
+
+    let max_speed_l = 150;
+    let max_speed_r = 140;
+
+    // set up the input
+    let stdin = process.stdin;
+    stdin.setRawMode(true);
+
+    let board = new five.Board({ port: '/dev/ttyUSB0' });
+
+    let l_motor = r_motor = null;
+
+    board.on("ready", function(err) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+        r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+
+        l_motor.forward(100);
+        r_motor.reverse(100);
+
+    })
 });
 
-app.get('/bot/left', function(req, res) {
-    res.sendFile(__dirname + '/public/left.js');
+app.get('/bot/left', function() {
+    let five = require("johnny-five");
+
+
+    let max_speed_l = 150;
+    let max_speed_r = 140;
+
+    // set up the input
+    let stdin = process.stdin;
+    stdin.setRawMode(true);
+
+    let board = new five.Board({ port: '/dev/ttyUSB0' });
+
+    let l_motor = r_motor = null;
+
+    board.on("ready", function(err) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+        r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+
+        l_motor.forward(100);
+        r_motor.forward(100);
+
+    })
 });
 
-app.get('/bot/right', function(req, res) {
-    res.sendFile(__dirname + '/public/right.js');
+app.get('/bot/right', function() {
+    let five = require("johnny-five");
+
+
+    let max_speed_l = 150;
+    let max_speed_r = 140;
+
+    // set up the input
+    let stdin = process.stdin;
+    stdin.setRawMode(true);
+
+    let board = new five.Board({ port: '/dev/ttyUSB0' });
+
+    let l_motor = r_motor = null;
+
+    board.on("ready", function(err) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+        r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+
+        l_motor.reverse(100);
+        r_motor.reverse(100);
+
+    })
 });
 
-app.get('/bot/stop', function(req, res) {
-    res.sendFile(__dirname + '/public/stop.js');
+app.get('/bot/stop', function() {
+    let five = require("johnny-five");
+
+
+    let max_speed_l = 150;
+    let max_speed_r = 140;
+
+    // set up the input
+    let stdin = process.stdin;
+    stdin.setRawMode(true);
+
+    let board = new five.Board({ port: '/dev/ttyUSB0' });
+
+    let l_motor = r_motor = null;
+
+    board.on("ready", function(err) {
+
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        l_motor = new five.Motor({ pins: { pwm: 6, dir: 7 } });
+        r_motor = new five.Motor({ pins: { pwm: 5, dir: 4 } });
+
+
+        l_motor.stop();
+        r_motor.stop();
+
+    })
 });
